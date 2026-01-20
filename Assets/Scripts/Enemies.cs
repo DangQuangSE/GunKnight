fe0fa4541111f2a737adb8fe4e31a3d43c22658e
DangQuangSE <quangndse180246@fpt.edu.vn>
2026-01-20 23:45:39 +0700

    Add Enemies script and update scene with new tilemaps
    Added Enemies.cs script and its meta file. Updated SampleScene.unity to include new Trees and Rock tilemaps, adjusted tilemap layers and sorting, and added a CompositeCollider2D to the Rock tilemap. Modified TagManager.asset to register new tags or layers as needed.

---
+++ Assets/Scripts/Enemies.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public abstract class Enemies : MonoBehaviour
+{
+    [SerializeField] protected float enemyMoveSpeed = 1f;
+    protected PlayerMovement player;
+    protected virtual void Start()
+    {
+        player = FindAnyObjectByType<PlayerMovement>();
+    }
+    protected virtual void Update()
+    {
+        MoveEnemyToPlayer();
+    }
+    protected void MoveEnemyToPlayer()
+    {
+        if (player != null)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, enemyMoveSpeed * Time.deltaTime);
+            FlipEnemy();
+        }
+
+    }
+    protected void FlipEnemy()
+    {
+        if (player != null)
+        {
+            transform.localScale = new Vector3(player.transform.position.x > transform.position.x ? -1 : 1, 1, 1);
+        }
+    }
+}
